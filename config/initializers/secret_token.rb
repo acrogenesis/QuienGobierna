650d6b636ef2386818a958a958debefb144a5990
acrogenesis <adrian.rangel@gmail.com>
2013-10-07 21:02:09 -0500

    crear rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuienGobierna::Application.config.secret_key_base = '43e7e2cc45758570951734363b57dfe14a00132c135644ad17c46508e404f9be6a7e303d06e9ab99dbb42ba10299e0179f2dc0def724b93337b38e944c88788b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuienGobierna::Application.config.session_store :cookie_store, key: '_QuienGobierna_session'
