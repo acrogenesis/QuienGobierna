74bd6e6cad1eacc7c1eb18f885a57c5765c3f24f
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-31 13:46:47 -0500

    temp secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-QuienGobierna::Application.config.secret_key_base = be4e9f968c2f5dd6d1d2ba8724371ea2c078a07062e28b53ea2bb8f441e4c4bc43bf059647e22b9f8a5b7a8b79ebde33448105eed37bfabc962d7b840b7f048b
+QuienGobierna::Application.config.secret_key_base = "be4e9f968c2f5dd6d1d2ba8724371ea2c078a07062e28b53ea2bb8f441e4c4bc43bf059647e22b9f8a5b7a8b79ebde33448105eed37bfabc962d7b840b7f048b"
