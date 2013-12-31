f37582fa49ea8fc1e9ab0e9a02a183b8e3e757d6
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-31 01:39:07 -0500

    virtual attribute image

--- app/models/governor.rb
+++ app/models/governor.rb
@@ -2,4 +2,7 @@ class Governor < ActiveRecord::Base
   def self.governor_list(state)
     where("state like '%#{state}%'")
   end
+  def image
+    "imggob/Th_" + self.name.delete(' ') + ".jpg"
+  end
 end
