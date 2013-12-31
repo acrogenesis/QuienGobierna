f204f72845e7ae92b659ab3891e3be8ce9f3b94f
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-31 00:24:05 -0500

    hacer metodos para sacar diputado

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,2 +1,7 @@
 class Deputy < ActiveRecord::Base
+  #attr_accessible :name, :state, :district, :email, :party, :image
+
+  def self.deputy_list(state,district)
+    where("district = #{district} AND state like '%#{state}%'")
+  end
 end
--- app/models/ife.rb
+++ app/models/ife.rb
@@ -1,2 +1,7 @@
 class Ife < ActiveRecord::Base
+  #attr_accessible :state, :nameState, :district, :city, :nameCity, :start, :finish, :total
+
+  def self.deputy_district(state,city,section)
+    Ife.where("state = #{state} AND city = #{city} AND start <= #{section} AND finish >= #{section} ")
+  end
 end
