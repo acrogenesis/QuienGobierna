fa307df682c6d7b130ccfba88138a44d30d0317e
Antonio Bastardo <antonbas@gmail.com>
2014-12-09 10:42:22 -0300

    Sugar syntax and interpolation better than concatenation

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -1,5 +1,5 @@
 class Deputy < ActiveRecord::Base
   def self.deputy_list(state, district)
-    where("district = #{district} AND state like '%#{state}%'")
+    where(district: district).where('state like ?', "'%#{state}%'")
   end
 end
