f16838a40db79c1112eb4aae59892432cdabd32a
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-09 08:25:05 -0600

    fix typo

--- app/models/ife.rb
+++ app/models/ife.rb
@@ -2,6 +2,6 @@ class Ife < ActiveRecord::Base
   # attr_accessible :state, :nameState, :district, :city, :nameCity, :start, :finish, :total
 
   def self.find_by_state_and_district_and_section(state, city, section)
-    lfe.where(state: state).where(city: city).where("start <= #{section} AND finish >= #{section}")
+    where(state: state).where(city: city).where("start <= #{section} AND finish >= #{section}")
   end
 end
